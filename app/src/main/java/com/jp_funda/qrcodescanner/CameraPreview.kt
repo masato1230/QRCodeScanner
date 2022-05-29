983ad1f8fc79b2402ab4b764d6187817534eabc0
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-05-29 10:21:32 +0900

    Add CameraPreview UseCase

---
+++ app/src/main/java/com/jp_funda/qrcodescanner/CameraPreview.kt
@@ -0,0 +1,52 @@
+package com.jp_funda.qrcodescanner
+
+import android.util.Log
+import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.view.PreviewView
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import kotlinx.coroutines.launch
+
+@Composable
+fun CameraPreview(modifier: Modifier) {
+    val coroutineScope = rememberCoroutineScope()
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    AndroidView(
+        modifier = modifier,
+        factory = { context ->
+            val previewView = PreviewView(context).apply {
+                this.scaleType = scaleType
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+            }
+            // CameraX Preview UseCase
+            val previewUseCase = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+
+            coroutineScope.launch {
+                val cameraProvider = context.getCameraProvider()
+                try {
+                    // use caseをライフサイクルにバインドする前にアンバインドを行う必要がある
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner, CameraSelector.DEFAULT_BACK_CAMERA, previewUseCase
+                    )
+                } catch (ex: Exception) {
+                    Log.e("CameraPreview", "Use case binding failed", ex)
+                }
+            }
+            previewView
+        }
+    )
+}
--- app/src/main/java/com/jp_funda/qrcodescanner/MainActivity.kt
+++ app/src/main/java/com/jp_funda/qrcodescanner/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.jp_funda.qrcodescanner.ui.theme.QRCodeScannerTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,5 +29,5 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainContent() {
-
+    CameraPreview(modifier = Modifier.fillMaxSize())
 }
